ff215c76fc66f9cc90bdbf0d7d51c32f902c4b5e
alexandershov <codumentary.com@gmail.com>
2023-06-02 12:48:27 +0400

    add virtual threads

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
 fun main() {
     checkBloomFilter()
+    // checkVirtualThreads intentionally contains infinite loop, that's why it's commented out
+    // checkVirtualThreads()
 }
---
+++ src/main/kotlin/VirtualThreads.kt
@@ -0,0 +1,32 @@
+import java.time.LocalTime
+
+fun checkVirtualThreads() {
+    val parallelism = System.getProperty("jdk.virtualThreadScheduler.parallelism")
+    val maxPoolSize = System.getProperty("jdk.virtualThreadScheduler.maxPoolSize")
+    val minRunnable = System.getProperty("jdk.virtualThreadScheduler.minRunnable")
+    println("parallelism = $parallelism, maxPoolSize = $maxPoolSize, minRunnable = $minRunnable")
+
+    val busyThread = Thread.ofVirtual().name("my thread").start {
+
+        while (true) {
+
+        }
+        println("${LocalTime.now()} cpu heavy thread is finished")
+    }
+
+    /*
+    If we start JVM with only one platform thread:
+     -Djdk.virtualThreadScheduler.parallelism=1
+     -Djdk.virtualThreadScheduler.maxPoolSize=1
+     -Djdk.virtualThreadScheduler.minRunnable=1
+    then printingThread will never start executing, because Java's virtual threads are cooperative
+
+    Good write up about it is here: https://blog.rockthejvm.com/ultimate-guide-to-java-virtual-threads/
+     */
+    val printingThread = Thread.ofVirtual().name("my thread").start {
+        println("${LocalTime.now()} i am a virtual thread!")
+    }
+
+    busyThread.join()
+    printingThread.join()
+}
