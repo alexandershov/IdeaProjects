0e5ad08a4a406c927c529b30dcc5a613e7dc8173
alexandershov <codumentary.com@gmail.com>
2023-06-11 13:26:04 +0400

    fortran: calculate sum of array

--- sog-fortran/hello.f
+++ sog-fortran/hello.f
@@ -1,3 +1,10 @@
-   10 FORMAT (5HHALLO)
-      PRINT 10
+      DIMENSION IA(9999)
+10    FORMAT (I4/(I4))
+      READ 10, I, (IA(J), J=1,I)
+      ISUM = 0
+      DO 20 J = 1,I
+      ISUM = ISUM + IA(J)
+20    CONTINUE
+30    FORMAT (I4)
+      PRINT 30, (ISUM)
       END
