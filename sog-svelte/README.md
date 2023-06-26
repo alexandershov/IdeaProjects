f6990caa9dd15954c12c8a10b14812d4d5fce0fe
alexandershov <codumentary.com@gmail.com>
2023-06-26 21:28:54 +0400

    svelte: add svelte tutorial

---
+++ sog-svelte/README.md
@@ -0,0 +1,16 @@
+Svelte is a component react-like library for js.
+Svelte is simpler than react and has a smaller runtime footprint.
+Sveltekit is a framework based on svelte.
+
+Create a new project in my-app directory using sveltekit.
+
+```bash
+npm create svelte@latest my-app
+```
+
+Install dependencies and start server
+
+```bash
+npm install
+npm run dev
+```
---
+++ sog-svelte/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<!-- you write scripts in <script> -->
+<script>
+    let count = 9;
+    // $: signifies a reactive line
+    // when count is changed, then doubled also changes
+    $: doubled = count * 2;
+
+    function incCount() {
+        count++
+    }
+
+    let doubled;
+</script>
+
+<!-- when values change, svelte does DOM updates efficiently-->
+<button on:click={incCount}>inc count</button>
+
+<div>count={count} doubled={ doubled }</div>
+
