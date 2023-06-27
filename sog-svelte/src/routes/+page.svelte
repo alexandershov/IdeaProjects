eaa1dde8e59d2e1c9af5006fd29431c2403e4322
alexandershov <codumentary.com@gmail.com>
2023-06-27 13:32:46 +0400

    svelte: add svelte inputs example & typescript support

--- sog-svelte/src/routes/+page.svelte
+++ sog-svelte/src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <!-- you write scripts in <script> -->
-<script>
-    let count = 9;
+<script lang="ts">
+    let a = 1;
+    let b = 2;
+    let count: number = 20;
     // $: signifies a reactive line
     // when count is changed, then doubled also changes
     $: doubled = count * 2;
@@ -17,3 +19,9 @@
 
 <div>count={count} doubled={ doubled }</div>
 
+<!-- When we change the inputs, then the values a and b change also-->
+<input type="number" bind:value={a}/>
+<input type="number" bind:value={b}/>
+
+<!-- change of a and b is automatically renders this div -->
+<div>{a} + {b} = {a + b}</div>
