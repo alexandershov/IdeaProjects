c158e1f629ca905f34d91ca892a75564d220e4dc
alexandershov <codumentary.com@gmail.com>
2023-07-31 21:09:14 +0400

    tools: add godot tutorial

---
+++ tools/godot/godot.md
@@ -0,0 +1,15 @@
+## Godot
+
+Godot is an open source game engine.
+
+Current directory is an example of godot project.
+
+Godot can create native games or can export to web.
+
+Project -> Export ... -> Web (select directory for export)
+
+And then serve the project using the serve.py python script
+
+```shell
+python3 serve.py --root {export_dir}
+```
---
+++ tools/godot/player.gd
@@ -0,0 +1,49 @@
+# GDScript is a language similar to Python
+
+extends Area2D
+
+
+# @export allows you to edit variable in the Node properties
+@export var speed = 400
+
+var screen_size
+
+
+func _ready():
+	# _ready() is called when a node enters the scene tree
+	screen_size = get_viewport_rect().size
+	
+	
+func _process(delta):
+	# _process is called on every frame
+	# delta is time elapsed from the previous call of _process
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed('move_down'):
+		velocity.y += 1
+	if Input.is_action_pressed('move_up'):
+		velocity.y -= 1
+	if Input.is_action_pressed('move_left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('move_right'):
+		velocity.x += 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		# $x is a shorthand for get_node('x')
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = 'walk'
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	if velocity.y != 0:
+		$AnimatedSprite2D.animation = 'up'
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+		$AnimatedSprite2D.flip_h = false
+		
+	
+		
