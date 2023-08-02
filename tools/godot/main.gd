494e7ba4508c575fc3db6eb632152570ca0532e2
alexandershov <codumentary.com@gmail.com>
2023-08-02 12:32:31 +0400

    tools: finish godot 2d tutorial

--- tools/godot/main.gd
+++ tools/godot/main.gd
@@ -6,7 +6,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,16 +17,24 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 	
 	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
+	$HUD.update_score(score)
+	$HUD.show_message('Get Ready')
+	get_tree().call_group('mobs', 'queue_free')
+	$Music.play()
+	
 
 func _on_score_timer_timeout():
 	score += 1	
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
