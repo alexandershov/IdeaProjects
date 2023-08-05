66174ef55584aa944838e2c06e788bed2bb328f6
alexandershov <codumentary.com@gmail.com>
2023-08-05 12:27:14 +0400

    leetcode: improve space complexity of https://leetcode.com/problems/decode-ways/

--- leetcode/decode_ways.py
+++ leetcode/decode_ways.py
@@ -4,7 +4,7 @@
 # 11:55 checked, no bugs
 
 
-class Solution:
+class SolutionRecursion:
     def numDecodings(self, s: str) -> int:
         return find_num_decodings(s, start=0)
 
@@ -29,3 +29,21 @@ def find_num_decodings(s: str, start: int, cache=None) -> int:
 
     cache[start] = result
     return result
+
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        two_before, one_before = 1, 1
+        cur_ways = 1
+        for i in range(len(s)):
+            cur_ways = 0
+            cur_char = s[i]
+            if cur_char != '0':
+                cur_ways += one_before
+            if i > 0:
+                prev_char = s[i - 1]
+                double_digits = f'{prev_char}{cur_char}'
+                if prev_char != '0' and int(double_digits) <= 26:
+                    cur_ways += two_before
+            two_before, one_before = one_before, cur_ways
+        return cur_ways
