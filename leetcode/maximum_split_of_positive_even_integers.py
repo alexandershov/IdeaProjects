50e5645c255111a1f5bd8dd8e4a922b27b3e1d80
alexandershov <codumentary.com@gmail.com>
2023-08-12 11:45:08 +0400

    leetcode: solve https://leetcode.com/problems/maximum-split-of-positive-even-integers/

---
+++ leetcode/maximum_split_of_positive_even_integers.py
@@ -0,0 +1,32 @@
+# 10:53 started reading
+# 10:55 started thinking
+# 11:13 trying to prove greedy
+# 11:32 implemented greedy, but don't understand why it'll give maximum split
+
+
+# ideas:
+# greedy
+# answer is basically the same for odd/problem, but divided by 2
+# dynamic programming
+
+# observation: if it's odd then there's no solution
+# [no]solution_even(2 * k) = map(2 * _, solution_odd(k))
+# [no]solution_odd(2 * k + 1) =
+
+
+class Solution:
+    def maximumEvenSplit(self, finalSum: int) -> list[int]:
+        result = []
+        if finalSum % 2 == 1:
+            return []
+
+        cur_sum = 0
+        smallest = 2
+        while cur_sum != finalSum:
+            next_smallest = smallest + 2
+            left = finalSum - (cur_sum + smallest)
+            if left >= next_smallest or left == 0:
+                result.append(smallest)
+                cur_sum += smallest
+            smallest = next_smallest
+        return result
