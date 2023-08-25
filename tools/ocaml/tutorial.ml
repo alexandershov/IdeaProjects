2b42da78db9ea5853909f68958fa2672e090ce95
alexandershov <codumentary.com@gmail.com>
2023-08-25 21:27:15 +0400

    tools: add more info on OCaml modules

--- tools/ocaml/tutorial.ml
+++ tools/ocaml/tutorial.ml
@@ -16,5 +16,20 @@ let three = (module Three : X_int);
 (* Functors have different meaning in OCaml.
    Functor is basically a function that converts module to another module.
    It's similar to C++ templates.
+   The difference is that we can write wrong C++ template that will be checked
+   during the instantiation time.
+
+   With functors we are getting typechecking during the functor definition.
  *)
 module Next (X : X_int) = struct let x = X.x + 1 end;;
+
+(*
+  We apply functors using () syntax
+  We can apply functors several times
+*)
+module Four = Next(Three);;
+module Five = Next(Next(Three));;
+
+print_int Four.x;
+print_endline "";
+print_int Five.x;
