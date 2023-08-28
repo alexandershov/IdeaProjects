5b3720106a84135f4aee9cd03a30c1f5159da3f7
alexandershov <codumentary.com@gmail.com>
2023-08-28 21:38:41 +0400

    tools: more coq

--- tools/coq/add_0.v
+++ tools/coq/add_0.v
@@ -7,3 +7,16 @@ Proof.
   (* `simpl` simplifies the left part of equation `0 + n` to `n` *)
   (* reflexivity is a fancy word for "x is equal x, duh" *)
 Qed.
+
+Theorem tuple_equality : forall (n m o p : nat),
+     n = m -> (n, o) = (n, p) -> (n, o) = (m, p).
+Proof.
+  (* eq1 is (n = m), eq2 is the second equation *)
+  intros n m o p eq1 eq2.
+
+  (* replace m with n in goal *)
+  rewrite <- eq1.
+
+  (* apply tactic `apply`, it's unclear what apply tactic does *)
+  apply eq2.
+Qed.
