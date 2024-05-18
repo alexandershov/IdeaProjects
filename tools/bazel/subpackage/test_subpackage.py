f5d0ea8cc352d32ad8cb9f1da302a789cd39e800
alexandershov <codumentary.com@gmail.com>
2024-05-18 15:55:33 +0200

    bazel: describe network sandbox

--- tools/bazel/subpackage/test_subpackage.py
+++ tools/bazel/subpackage/test_subpackage.py
@@ -1,4 +1,6 @@
 import unittest
+import urllib.error
+import urllib.request
 
 import adder
 
@@ -6,6 +8,11 @@ import adder
 class TestCase(unittest.TestCase):
     def test_add(self):
         self.assertEqual(adder.add(1, 2), 3)
+
+    def test_block_network_tag(self):
+        with self.assertRaises(urllib.error.URLError):
+            with urllib.request.urlopen("http://google.com") as response:
+                print(response.read())
 
 
 if __name__ == '__main__':
