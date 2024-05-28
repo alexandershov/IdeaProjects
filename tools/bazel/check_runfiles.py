54aa41db0d9d702809a35be21e165973e46448f8
alexandershov <codumentary.com@gmail.com>
2024-05-28 22:54:24 +0200

    bazel: describe rlocationpath

--- tools/bazel/check_runfiles.py
+++ tools/bazel/check_runfiles.py
@@ -1,10 +1,19 @@
+import argparse
 import os
 import pathlib
 
 import requests
+import runfiles
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--rlocationpath', required=True)
+    return parser.parse_args()
 
 
 def main():
+    args = parse_args()
     # cwd is bazel-out/..../_main
     # _main is ctx.workspace_name, looks like it can't be changed
     print(f'{os.getcwd()=}')
@@ -13,6 +22,9 @@ def main():
     print(f'{os.listdir()=}')
     assert pathlib.Path('bazel.md').exists()
     assert pathlib.Path('subpackage/file_in_subpackage.txt').exists()
+    r = runfiles.Create()
+    location = pathlib.Path(r.Rlocation(args.rlocationpath))
+    assert location.exists()
 
 
 if __name__ == '__main__':
