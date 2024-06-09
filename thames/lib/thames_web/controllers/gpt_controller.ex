0417e1da62d0c2302695c2c55ed23f9c21739500
alexandershov <codumentary.com@gmail.com>
2024-06-09 12:42:05 +0200

    proxy request to openai

--- thames/lib/thames_web/controllers/gpt_controller.ex
+++ thames/lib/thames_web/controllers/gpt_controller.ex
@@ -1,7 +1,16 @@
 defmodule ThamesWeb.GPTController do
   use ThamesWeb, :controller
 
-  def chat(conn, _params) do
-    render(conn, :chat, layout: false)
+  def chat(conn, params) do
+    answer = if params["query"] do
+         response = Req.post!("https://api.openai.com/v1/chat/completions",
+                              json: %{model: "gpt-4o",
+                                      messages: [%{role: "user", content: params["query"]}]},
+                              auth: {:bearer, System.get_env("OPENAI_API_KEY")})
+         List.first(response.body["choices"])["message"]["content"]
+    else
+      ""
+    end
+    render(conn, :chat, answer: answer, query: params["query"], layout: false)
   end
 end
