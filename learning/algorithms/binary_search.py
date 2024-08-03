b08d9e90dd82162ac044bd777486d521dea2baf7
alexandershov <codumentary.com@gmail.com>
2024-08-03 19:56:30 +0200

    algorithms: implement binary_search_left

--- learning/algorithms/binary_search.py
+++ learning/algorithms/binary_search.py
@@ -5,6 +5,7 @@ def binary_search(seq, value):
     start = 0
     end = len(seq)
     # [start, end) shrinks at every iteration
+    # invariant is that we're not interested in items < start and in items >= end
     while start < end:
         mid = (start + end) // 2
         if seq[mid] == value:
@@ -14,6 +15,33 @@ def binary_search(seq, value):
         else:
             start = mid + 1
     return -1
+
+
+def binary_search_left(seq, value):
+    """
+    Return index of first occurrence of value in seq.
+    If value is not in seq, return index where value would be located after insertion
+    """
+    start = 0
+    end = len(seq)
+    # invariants are:
+    # * everything after and including `end` is >= value
+    # * everything before start is < value
+    while start < end:
+        mid = (start + end) // 2
+        if seq[mid] >= value:
+            end = mid
+        else:
+            start = mid + 1
+    # at the end start == end
+    # invariants are still valid at this point,
+    # so everything after and including end is >= value
+    # everything before start (== end) is < value
+    # this means that end (== start) index is the first where we have >= value
+    # if value is in seq, then start is the index of first occurrence
+    # if value is not in seq, then start is the first index that is > value
+    # this means that start is index of value after insertion
+    return start
 
 
 @pytest.mark.parametrize('items, value, expected', [
@@ -26,3 +54,14 @@ def binary_search(seq, value):
 ])
 def test_binary_search(items, value, expected):
     assert binary_search(items, value) == expected
+
+
+@pytest.mark.parametrize('items, value, expected', [
+    ('abbbde', ' ', 0),
+    ('abbbde', 'a', 0),
+    ('abbbde', 'b', 1),
+    ('abbbde', 'c', 4),
+    ('abbbde', 'f', 6),
+])
+def test_binary_search_left(items, value, expected):
+    assert binary_search_left(items, value) == expected
