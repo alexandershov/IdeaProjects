efa4700cd1d9f9a623922063af40df2da6f1350b
alexandershov <codumentary.com@gmail.com>
2024-08-03 20:08:26 +0200

    algorithms: implement binary_search_right

--- learning/algorithms/binary_search.py
+++ learning/algorithms/binary_search.py
@@ -44,6 +44,31 @@ def binary_search_left(seq, value):
     return start
 
 
+def binary_search_right(seq, value):
+    """
+    Return max index where value would be located after insertion
+    """
+    start = 0
+    end = len(seq)
+    # invariants are:
+    # * everything after and including `end` is > value
+    # * everything before start is <= value
+    while start < end:
+        mid = (start + end) // 2
+        if seq[mid] > value:
+            end = mid
+        else:
+            start = mid + 1
+    # at the end start == end
+    # invariants are still valid at this point,
+    # so everything after and including end is > value
+    # everything before start (== end) is <= value
+    # so end (== start) is the first index where seq[end] > value
+    # if value is in seq, then it's 1 + index of last occurrence
+    # if value is not in seq then it's just insertion point
+    return start
+
+
 @pytest.mark.parametrize('items, value, expected', [
     ('abcde', 'f', -1),
     ('abcde', 'a', 0),
@@ -65,3 +90,14 @@ def test_binary_search(items, value, expected):
 ])
 def test_binary_search_left(items, value, expected):
     assert binary_search_left(items, value) == expected
+
+
+@pytest.mark.parametrize('items, value, expected', [
+    ('abbbde', ' ', 0),
+    ('abbbde', 'a', 1),
+    ('abbbde', 'b', 4),
+    ('abbbde', 'c', 4),
+    ('abbbde', 'f', 6),
+])
+def test_binary_search_right(items, value, expected):
+    assert binary_search_right(items, value) == expected
