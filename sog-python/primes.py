b1de4cb953ec26d9fc1ca8bc7204ab89712419d1
alexandershov <codumentary.com@gmail.com>
2025-03-02 18:10:39 +0100

    python: implement less naive version of prime generator

--- sog-python/primes.py
+++ sog-python/primes.py
@@ -15,7 +15,19 @@ def naive_primes() -> Generator[int, None, None]:
 
 def primes() -> Generator[int, None, None]:
     generated = [2]
+    divisor_end = len(generated)
     yield from generated
+    for x in itertools.count(3, step=2):
+
+        while divisor_end < len(generated) and generated[divisor_end] ** 2 <= x:
+            divisor_end += 1
+
+        for prime_divisor in itertools.islice(generated, 1, divisor_end):
+            if x % prime_divisor == 0:
+                break
+        else:
+            generated.append(x)
+            yield x
 
 
 @pytest.mark.parametrize('n', [10])
