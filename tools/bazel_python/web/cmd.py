e7fd47683db668768d88f674099cb23e2b5e17c4
alexandershov <codumentary.com@gmail.com>
2025-06-05 23:45:07 +0200

    describe runfiles

---
+++ tools/bazel_python/web/cmd.py
@@ -0,0 +1,16 @@
+import os
+
+
+def main():
+    # bazel-bin/web/cmd is a bash binary
+    # bazel-bin/web/cmd.runfiles are runfiles
+    # bazel-bin/web/cmd executes bazel-bin/web/cmd.runfiles/_main/web/cmd.py
+    # there's bazel-bin/web/cmd.runfiles/_main/web/files/data.txt
+    # that's why the approach with os.path.join finds the data.txt during the runtime
+    path = os.path.join(os.path.dirname(__file__), "files/data.txt")
+    with open(path) as fileobj:
+        print(fileobj.read())
+
+
+if __name__ == '__main__':
+    main()
