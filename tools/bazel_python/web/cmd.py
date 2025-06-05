e182592154904f2add60c1cf760eca2a2eadc53d
alexandershov <codumentary.com@gmail.com>
2025-06-05 23:51:52 +0200

    use runfiles

--- tools/bazel_python/web/cmd.py
+++ tools/bazel_python/web/cmd.py
@@ -1,5 +1,6 @@
 import os
 
+import runfiles
 
 def main():
     # bazel-bin/web/cmd is a bash binary
@@ -9,6 +10,14 @@ def main():
     # that's why the approach with os.path.join finds the data.txt during the runtime
     path = os.path.join(os.path.dirname(__file__), "files/data.txt")
     with open(path) as fileobj:
+        print(fileobj.read())
+
+    # runfiles libraries can resolve runfiles paths
+    r = runfiles.Runfiles.Create()
+    # we pass _main/{path in a repo} to Rlocation
+    rlocation = r.Rlocation("_main/web/files/data.txt")
+    print(rlocation)
+    with open(rlocation) as fileobj:
         print(fileobj.read())
 
 
