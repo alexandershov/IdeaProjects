10eef1fb4056e9bc67d78b4d12ea08b7726bbb1f
alexandershov <codumentary.com@gmail.com>
2025-06-15 17:26:11 +0200

    bazel: add --string-to-append to py_cat.py

--- tools/bazel/subpackage/py_cat.py
+++ tools/bazel/subpackage/py_cat.py
@@ -1,18 +1,20 @@
 import argparse
 import contextlib
+import itertools
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', type=argparse.FileType('r'))
-    parser.add_argument('--output', type=argparse.FileType('w'))
+    parser.add_argument('--input', type=argparse.FileType('r'), required=True)
+    parser.add_argument('--output', type=argparse.FileType('w'), required=True)
+    parser.add_argument('--string-to-append', default='')
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
     with contextlib.closing(args.input) as input_, contextlib.closing(args.output) as output:
-        for line in input_:
+        for line in itertools.chain(input_, [args.string_to_append]):
             print(line, file=output, end="")
 
 
