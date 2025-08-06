70c7fad68b4c4f7841e5e1f9af47d2e6134267c6
alexandershov <codumentary.com@gmail.com>
2025-08-06 21:16:07 +0200

    zig: describe optional, slices, and payload captures

--- tools/zig/main.zig
+++ tools/zig/main.zig
@@ -33,10 +33,40 @@ pub fn main() void {
 
     std.debug.print("arr.len = {}\n", .{arr.len});
 
+    // slices are pointers that also contain count of elements that they point to
+    const slice = arr[3..];
+    std.debug.print("slice.length = {}\n", .{slice.len});
+
     for (arr, 0..) |item, index| {
         std.debug.print("arr[{}] = {}\n", .{ index, item });
     }
 
     std.debug.print("divide(3, 2) = {any}\n", .{divide(3, 2)});
+
     std.debug.print("divide(3, 0) = {any}\n", .{divide(3, 0)});
+
+    // catch allows fallback to default value in case of error
+    const divisionByZero = divide(3, 0) catch 666;
+
+    // kinda like pattern matching on error union, else with err capture is required
+    if (divide(3, 2)) |v| {
+        std.debug.print("divide(3, 2) = {}\n", .{v});
+    } else |err| {
+        std.debug.print("divide(3, 2) = {}\n", .{err});
+    }
+
+    std.debug.print("divisionByZero = {any}\n", .{divisionByZero});
+
+    // optional
+    const optCount: ?u32 = 32;
+    // orelse unwraps optional
+    std.debug.print("optCount = {any}\n", .{optCount orelse 18});
+    // unconditinal unwrap
+    std.debug.print("optCount + 1 = {}\n", .{(optCount orelse unreachable) + 1});
+    // shorthand for unconditinal unwrap
+    std.debug.print("optCount + 1 = {}\n", .{(optCount.?) + 1});
+    // payload capture, v is an u32
+    if (optCount) |v| {
+        std.debug.print("v = {}\n", .{v});
+    }
 }
