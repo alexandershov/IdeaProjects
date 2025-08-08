8ce3cc500ce78d60cf52ee91cce0c1b1dfc766a4
alexandershov <codumentary.com@gmail.com>
2025-08-08 21:10:12 +0200

    zig: stab at designing parser

---
+++ tools/zig/devlog.md
@@ -0,0 +1,14 @@
+## 2025-08-08
+
+Possible approaches:
+* Tokenize and Parse 
+     
+* No tokenize
+  seems hacky and implementation will be harder
+```zig
+  pub fn parsePgn(reader) {
+    while (!eof) {
+        parseGame()
+    }  
+}
+```
--- tools/zig/pgn_parser.zig
+++ tools/zig/pgn_parser.zig
@@ -1,5 +1,26 @@
 const std = @import("std");
 
+// pgn format spec:
+// comments: ; till the end of string and {} comments
+// line starting with % should be ignored
+
+
+const Token = union(enum) {
+    .Value: []u8,
+    
+    .Number: u32,
+    .Symbol: []u8,
+    .Comment: []u8,
+    .String: []u8,
+}
+
+const Tokenizer = struct {
+    reader: std.io.Reader,
+
+    fn next() !?Token {}
+}
+
+
 pub fn main() !void {
     const stdin = std.io.getStdIn().reader();
     const stdout = std.io.getStdOut().writer();
