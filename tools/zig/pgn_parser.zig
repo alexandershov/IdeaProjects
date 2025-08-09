db5261ec1bd63384edba6485c5b5bf6967fda457
alexandershov <codumentary.com@gmail.com>
2025-08-09 12:25:07 +0200

    zig: finish designing Tokenizer interface

--- tools/zig/pgn_parser.zig
+++ tools/zig/pgn_parser.zig
@@ -16,18 +16,18 @@ const std = @import("std");
 /// * .String("Live Chess")
 /// * .ClosingBracket("]")
 const Token = union(enum) {
-    /// Delimit PGN tags
+    /// Delimits PGN tags
     .OpeningBracket: u8,
     .ClosingBracket: u8,
 
-    /// Represent tag names
+    /// Represents tag names
     .Symbol: []u8,
 
-    /// Delimit (possibly recursive) variations
+    /// Delimits (possibly recursive) variations
     .OpeningParen: u8,
     .ClosingParen: u8,
 
-    /// Represent move numbers
+    /// Represents move numbers
     .Number: u32,
 
     /// Comments can be of two types:
@@ -35,21 +35,43 @@ const Token = union(enum) {
     /// { comment inside of the braces }
     .Comment: []u8,
 
-    
+    /// Includes spaces, tabs, newlines
     .Whitespace: []u8,
 
-    /// Represent tag values
+    /// Represents tag values
     .String: []u8,
 
+    /// Represents period after a white move number `1. e4`
+    .Period u8,
+
+    /// Represents period after a black move number `1... e5`
+    .TriplePeriod []u8,
+
+    /// Represents the move, e.g. "Nf3" or "O-O-O".
+    /// Note that castle is represented with capital letter O, not zero (0)
+    /// That's part of PGN spec
+    .Move []u8,
+
+    /// Represents Numeric Annotation Glyph, e.g. "$2" in "1. e4 $2"
+    .MoveAnnotation []u8
+
+    /// "1-0", "0-1", "1/2-1/2", or "*" (unknown result)
+    /// Follows the 
+    .Result []u8,
 }
 
 /// Convert a stream of bytes into Tokens
+/// Iterator
 const Tokenizer = struct {
-    ///
+    /// Contains PGN data
     reader: std.io.Reader,
 
-    /// 
-    fn next() !?Token {}
+    /// Returns next token in a stream and moves the iterator further.
+    /// Standard iterator stuff.
+    fn next(self *Tokenizer) !?Token {}
+
+    /// Returns next token in a stream but iterator stays at the same position.
+    fn peek(self *Tokenizer) !?Token {}
 }
 
 
