fa00208d3d1625739eb9043f77e2e86a5ec595f2
alexandershov <codumentary.com@gmail.com>
2025-08-11 20:39:12 +0200

    pgn_parser: introduce BufferedReader
    so we can peek

--- tools/zig/pgn_parser.zig
+++ tools/zig/pgn_parser.zig
@@ -36,11 +36,10 @@ const Token = union(enum) {
     /// Represents tag values
     String: []u8,
 
-    /// Represents period after a white move number `1. e4`
+    /// Represents period e.g after a white move number `1. e4`
+    /// or one of the periods after a black move number `1... e5`
+    /// `...` is represented as three consequtive Period tokens
     Period: u8,
-
-    /// Represents periods after a black move number `1... e5`
-    TriplePeriod: []u8,
 
     /// "1-0", "0-1", "1/2-1/2", or "*" (star means unknown result)
     Result: []u8,
@@ -54,18 +53,78 @@ const Token = union(enum) {
     Text: []u8,
 };
 
+
+const BufferedReader = struct {
+    .buffer: u8[],
+    
+    /// Skips the next n bytes in the stream
+    /// Asserts that buffer contains at least n 
+    pub fn toss(self *BufferedReader, n: usize) ! {}
+    
+    /// Returns the next buffer.len bytes from the current position
+    /// Doesn't change the seek position
+    pub fn peek(self *BufferedReader, buffer []u8) !usize {}
+}
+
 /// Convert a stream of bytes into Tokens
 /// Iterator
 const Tokenizer = struct {
     /// Contains PGN data
-    reader: std.io.Reader,
+    reader: BufferedReader,
+
+    /// Used to allocate memory for tokens that don't have fixed length (e.g. comments, moves)
+    allocator: std.mem.Allocator,
+
+    ///
+    pub fn init(reader: BufferedReader, allocator: std.mem.Allocator) Tokenizer {
+        self.reader = reader;
+        self.allocator = allocator;
+    }
 
     /// Returns next token in a stream and moves the iterator further.
     /// Standard iterator stuff.
     fn next(_: *Tokenizer) !?Token {}
 
     /// Returns next token in a stream but iterator stays at the same position.
-    fn peek(_: *Tokenizer) !?Token {}
+    fn peek(self: *Tokenizer) !?Token {
+        const drawResult ["1/2-1/2".len]u8 = undefined;
+        const decisiveResult ["1-0".len]u8 = undefined;
+
+        reader.peek(drawResult);
+        if (drawResult == "1/2-1/2") return Token.Result{"1/2-1/2"};
+
+        reader.peek(decisiveResult);
+        if (decisiveResult == "1-0") return Token.Result{"1-0"};
+        if (decisiveResult == "0-1") return Token.Result{"0-1"};
+
+        const nextChar [1]u8 = undefined;
+        reader.peek(nextChar);
+        reader.toss(1);
+        switch (nextChar[0]) {
+            '*' => {
+                reader.toss(1);
+                return Token.Result{"*"};
+            }
+            ';' => {
+                // read until new line
+                return Token.Comment{};
+            }
+            '{' => {
+                // read until }
+                return Token.Comment{};
+            }
+            '0'..'9' => {
+                // read while digit
+                return Token.Number{};
+            }
+            '.' => return Token.Period{'.'};
+            '(' => return Token.OpeningParen{'('};
+            ')' =>                 return Token.ClosingParen{')'};
+            '[' =>                 return Token.OpeningBracket{'['};
+            ']' =>                 return Token.ClosingBracket{']'};
+            ' ', '\t', '\n' => return Token.Whitespace{nextChar[0]};
+        }
+    }
 };
 
 pub fn main() !void {
