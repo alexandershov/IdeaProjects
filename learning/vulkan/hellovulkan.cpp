186e516e0acfa4b8bba7a6e4623c62ddc6e43da2
alexandershov <codumentary.com@gmail.com>
2025-08-29 23:37:00 +0200

    learning: create window with glfw

--- learning/vulkan/hellovulkan.cpp
+++ learning/vulkan/hellovulkan.cpp
@@ -1,35 +1,29 @@
+#include <iostream>
+
+// with GLFW_INCLUDE_VULKAN glfw will include vulkan headers
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
-#include <iostream>
-
 int main() {
+    // init window
     glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, /* don't create OpenGL context */ GLFW_NO_API);
+    // not resizable window, so we don't need to deal with resize
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+    auto window = glfwCreateWindow(/* width */ 800, /* height */ 600, /* title */ "Hello Vulkan",
+        /* monitor, like a display */ nullptr, /* OpenGL-specific */ nullptr);
 
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while(!glfwWindowShouldClose(window)) {
+    // main loop
+    while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
     }
 
+    // deinit window
     glfwDestroyWindow(window);
-
+    // deinit glfw
     glfwTerminate();
 
-    return 0;
+    std::cout << "done!\n";
 }
