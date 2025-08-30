61c92e36c7f35d9c72f9d6ee96e422906489d0b2
alexandershov <codumentary.com@gmail.com>
2025-08-30 18:54:06 +0200

    vulkan: create shader pipeline

--- learning/vulkan/hellovulkan.cpp
+++ learning/vulkan/hellovulkan.cpp
@@ -418,6 +418,21 @@ int main() {
     result = vkCreateShaderModule(device, &fragShaderCreateInfo, nullptr, &fragShaderModule);
     checkedVk(result, "can't create frag shader module");
 
+    // creating pipeline out of shaders
+    VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
+    vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+    vertShaderStageInfo.module = vertShaderModule;
+    vertShaderStageInfo.pName = "main";
+
+    VkPipelineShaderStageCreateInfo fragShaderStageInfo{};
+    fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+    fragShaderStageInfo.module = fragShaderModule;
+    fragShaderStageInfo.pName = "main";
+
+    VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
+
     // main loop
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
