bbfa3f79252177439a32e4947acbc595601cbca6
alexandershov <codumentary.com@gmail.com>
2026-06-14 12:37:44 +0200

    erlang: describe hot reload

--- tools/erlang/tutorial.erl
+++ tools/erlang/tutorial.erl
@@ -1,33 +1,42 @@
 -module(tutorial).
 
 % /0 is function arity
--export([main/0, sum/2]).
+-export([main/0, sum/2, adder_loop/0]).
 
 sum(X, Y) ->
     X + Y.
 
+adder_loop() ->
+    receive
+        % this is pattern matching, add is atom; From, X, Y are variables
+        {add, From, X, Y} ->
+            io:format("adder: received [~p, ~p, ~p]~n", [From, X, Y]),
+            From ! {ok, X + Y},
+            % We call ourselves recursively, so this process forever handles messages
+            % by calling ourselves with ?MODULE:adder_loop this code is hot reload friendly:
+            % connect to a running program: erl -sname debug -setcookie secret -remsh myapp
+            % then run c(tutorial) and you'll get hot code reload
+            % without ?MODULE we'll continue executing old version of the adder_loop
+            ?MODULE:adder_loop()
+    end.
+
 main() ->
     % spawn creates a lightweight process, it's not an OS process, it's managed by Erlang VM (called BEAM)
-    AdderPid = spawn(fun Loop() ->
-        receive
-            % this is pattern matching, add is atom; From, X, Y are variables
-            {add, From, X, Y} -> From ! {ok, X + Y},
-            % We call ourselves recursively, so this process forever handles messages
-            Loop()
-        end
-    end),
+    AdderPid = spawn(fun() -> adder_loop() end),
 
-    spawn(fun() ->
+    spawn(fun Loop() ->
         % ! sends a message to pid, it sends a message asynchronously
         % self() is pid of the current erlang process
         % processes interact by sending messages to each other
         AdderPid ! {add, self(), 3, 2},
+        AdderPid ! {add, self(), 6, 7},
         io:format("sent add~n"),
         receive
             Msg ->
                 io:format("before~n"),
                 io:format("received ~p~n", [Msg]),
-                io:format("after~n")
+                io:format("after~n"),
+                Loop()
             % {ok, Result} -> io:format("Result = ~B~n", [Result])
         end
     end),
@@ -36,7 +45,7 @@ main() ->
         Msg ->
             io:format("[main] received ~p~n", [Msg])
     after
-        % this will trigger if no messages arrived in 100ms
+        % this will trigger if no messages arrived in 100000ms
         100000 ->
             io:format("timeout!~n")
     end,
