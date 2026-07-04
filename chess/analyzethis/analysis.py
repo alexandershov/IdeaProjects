a8df45c8bbe4b8b7d129ec714c48ae3e286d5ae6
alexandershov <codumentary.com@gmail.com>
2026-07-04 16:34:46 +0200

    chess: add subparsers

--- chess/analyzethis/analysis.py
+++ chess/analyzethis/analysis.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 import json
 import os
@@ -7,12 +8,15 @@ import dotenv
 import httpx
 
 
-async def download_games(lichess_user_name: str, lichess_api_token: str, max: int, output: pathlib.Path) -> None:
-    with output.open("w") as fileobj:
+async def download_games(args) -> None:
+    dotenv.load_dotenv()
+    lichess_api_token = os.environ["LICHESS_API_TOKEN"]
+    lichess_user_name = os.environ["LICHESS_USER_NAME"]
+    with args.output.open("w") as fileobj:
         async with httpx.AsyncClient() as client:
             response = await client.get(
                 f"https://lichess.org/api/games/user/{lichess_user_name}",
-                params={"max": max, "pgnInJson": "true"},
+                params={"max": args.max, "pgnInJson": "true"},
                 headers={
                     "Accept": "application/x-ndjson",
                     "Authorization": f"Bearer {lichess_api_token}"
@@ -22,12 +26,27 @@ async def download_games(lichess_user_name: str, lichess_api_token: str, max: in
                 fileobj.write(pgn)
 
 
+async def analyze_games(args):
+    raise RuntimeError("not implemented yet")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(required=True)
+    download_parser = subparsers.add_parser("download")
+    download_parser.add_argument("--max", type=int, default=5)
+    download_parser.add_argument("--output", default='/dev/stdout', type=pathlib.Path)
+    download_parser.set_defaults(func=download_games)
+
+    analysis_parser = subparsers.add_parser("analysis")
+    analysis_parser.add_argument("pgn")
+    analysis_parser.set_defaults(func=analyze_games)
+    return parser.parse_args()
+
+
 async def amain():
-    dotenv.load_dotenv()
-    lichess_api_token = os.environ["LICHESS_API_TOKEN"]
-    lichess_user_name = os.environ["LICHESS_USER_NAME"]
-    output = pathlib.Path(f"{lichess_user_name}-games.pgn")
-    await download_games(lichess_user_name, lichess_api_token, 5, output)
+    args = parse_args()
+    await args.func(args)
 
 
 if __name__ == '__main__':
