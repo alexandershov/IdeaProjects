cc067809d8afbc1cfc3ff3e4a436386a415895da
alexandershov <codumentary.com@gmail.com>
2026-07-18 12:42:40 +0200

    opengl: add grayscale postprocessing effect

--- learning/opengl/src/quad_fragment_shader.glsl
+++ learning/opengl/src/quad_fragment_shader.glsl
@@ -5,6 +5,10 @@ out vec4 Color;
 uniform sampler2D ourTexture;
 
 void main() {
-  Color = texture(ourTexture, TexCoord);
+  // we jump through the hoops of rendering via framebuffer texture, so we can add easily add post-processing in a second pass
+  // e.g. render in grayscale
+  vec4 FragColor = texture(ourTexture, TexCoord);
+  float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;
+  Color = vec4(average, average, average, 1.0);
 }
 
