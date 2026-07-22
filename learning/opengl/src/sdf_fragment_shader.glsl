365c227eb61e2ea7aad567788cc862b5058bb194
alexandershov <codumentary.com@gmail.com>
2026-07-22 23:55:03 +0200

    opengl: draw circle with fragment shader

--- learning/opengl/src/sdf_fragment_shader.glsl
+++ learning/opengl/src/sdf_fragment_shader.glsl
@@ -3,6 +3,11 @@ in vec4 Color;
 out vec4 FragColor;
 
 void main() {
-  FragColor = vec4(1.0, 0.0, 0.0, 0.0);
+     vec2 offset = vec2(300, 300);
+     if (dot(gl_FragCoord.xy - offset, gl_FragCoord.xy - offset) < 100 * 100) {
+     	discard;
+     }
+     
+     FragColor = vec4(1.0, 0.0, 0.0, 0.0);
 }
 
