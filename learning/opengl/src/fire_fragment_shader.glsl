ebd9565edbb5db31c17ff008ba8a144331c72f3a
alexandershov <codumentary.com@gmail.com>
2026-08-01 12:29:04 +0200

    opengl: add comment on mix in fire fragment shader

--- learning/opengl/src/fire_fragment_shader.glsl
+++ learning/opengl/src/fire_fragment_shader.glsl
@@ -4,10 +4,13 @@ in vec4 ParticleColor;
 
 out vec4 Color;
 
-uniform sampler2D ourTexture;
+uniform sampler2D fireSprite;
 
 void main() {
-  vec4 tex = texture(ourTexture, TexCoord);
+  vec4 tex = texture(fireSprite, TexCoord);
+  // fireSprite is a white sprite, so tex.r is intensity of it
+  // if tex.r is 0, then we're at the least intense part (edge) = mix will return ~red
+  // if tex.r is 1, then we're at the most intense part (core): mix will return ~yellow
   vec3 fireColor = mix(vec3(1.0, 0.1, 0.0, 1.0), vec3(1.0, 1.0, 0.3, 1.0), tex.r);
   Color = fireColor * ParticleColor;
 }
