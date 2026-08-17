a037e7123d7550e54c8cb48144853222b466fc74
alexandershov <codumentary.com@gmail.com>
2026-08-17 20:38:09 +0200

    raylib: implement vignette

--- tools/raylib/vignette.fs
+++ tools/raylib/vignette.fs
@@ -1,7 +1,8 @@
 #version 330
 
-// see aerial_perspective.fs for an explanation of raylib's built-in in/uniform/out parameters
+// vignette shader - points that are more farther from the center are darker
 
+// see aerial_perspective.fs for an explanation of raylib's built-in in/uniform/out parameters
 in vec2 fragTexCoord;
 in vec4 fragColor;
 
@@ -12,5 +13,13 @@ out vec4 finalColor;
 
 void main() {
     vec4 color = texture(texture0, fragTexCoord) * colDiffuse * fragColor;
-    finalColor = color;
+    // range of texture is [0; 1] in both directions, so center is (0.5, 0.5)
+    vec2 center = vec2(0.5, 0.5);
+    float dist = distance(fragTexCoord, center);
+    float maxDist = distance(center, vec2(1, 1));
+    float minBrightness = 0.0;
+    float maxBrightness = 1.0;
+    // lerp in the vignette space based on the relative distance, dist/distance is between 0 and 1
+    float brightness = mix(minBrightness, maxBrightness, 1 - dist/maxDist);
+    finalColor = vec4(color.rgb * brightness, color.a);
 }
